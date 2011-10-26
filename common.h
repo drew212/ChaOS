9dfc3060635574861a51ee15cc4994913f322c15
Drew Cross <mstngdrew212@gmail.com>
2011-10-26 10:22:57 -0500

    Changed some templating and added to GDT

--- common.h
+++ common.h
@@ -7,17 +7,17 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-typedef unsigned int    u32int;
-typedef          int    s32int;
-typedef unsigned short  u16int;
-typedef          short  s16int;
-typedef unsigned char   u8int;
-typedef          char   s8int;
+typedef unsigned int    uint32_t;
+typedef          int    int32_t;
+typedef unsigned short  uint16_t;
+typedef          short  int16_t;
+typedef unsigned char   uint8_t;
+typedef          char   int8_t;
 
 
-void outb(u16int port, u8int value);
-u8int inb(u16int port);
-u16int inw(u16int port);
+void outb(uint16_t port, uint8_t value);
+u8int inb(uint16_t port);
+u16int inw(uint16_t port);
 
 #endif
 
--- tables.c
+++ tables.c
@@ -6,13 +6,33 @@
  * Drew Cross <mstngdrew212@gmail.com>
  *
  */
+#include "common.h"
 
+extern void set_gdt(u32int_t);
 
 struct gdt_entry {
 
 } __attribute__((packed));
 
-struct gdt_table_ptr {
+struct gdt_ptr {
     unsigned short limit;
     unsigned int base;
 } __attribute__((packed));
+
+struct gdt_entry gdt[5];
+struct gdt_ptr;
+
+void gdt_flush();
+
+// Initializes the descriptor tables
+void init_descriptor_tables()
+{
+    init_gdt();
+}
+
+// Initializes the global descriptor table
+void init_gdt()
+{
+
+
+}
